0804499cc78b0797392a233e093cfba6dd64a1e6
AzureADBłażejSzajrych <bs@macrix.pl>
2022-09-28 15:48:31 +0200

    Add project files.

---
+++ CommandParameterNullInDataTemplate/BooleanCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Markup;
+
+namespace CommandParameterNullInDataTemplate
+{
+  public class BooleanCommand : MarkupExtension, ICommand
+  {
+    public event EventHandler CanExecuteChanged;
+    public bool CanExecute(object parameter)
+    {
+      if (parameter is Boolean b)
+      {
+        return true;
+      }
+
+      return false;
+    }
+    public void Execute(object parameter)
+    {
+    }
+    public override object ProvideValue(IServiceProvider serviceProvider)
+      => this;
+  }
+}
